08a9b27030612e16e1adab76324c65ce5b9ec788
Melissa Reynolds <mkreynolds@cableone.net>
2017-01-13 02:33:12 +0000

    Added message.js to add new message

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,1 +1,13 @@
 // We want to be able to add a new message and have it update the screen right away
+
+$(function(){
+
+  $("#send-message").click(function(){
+    console.log("Send was clicked");
+
+    var receiverUserId = $("#message_receiver_receiver_id").val();
+
+    var messageBody = $("#messsage-body").val();
+    console.log("Receiver:", receiverUserId, "Message:", messageBody)
+  });
+});
